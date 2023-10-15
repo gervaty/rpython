49ce4a011dcd1530497208f33f5e30ad25acbc98
Artem Sokolov <artem.sokolov@bp.com>
2023-10-15 21:02:58 +0500

    pwdgen v 1.0

---
+++ pwdgen.py
@@ -0,0 +1,20 @@
+import random
+print("pwdgen 1.0 - universal password generator.")
+
+symbols = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$%^&*()"
+while True:
+    password = ""
+    numStr = input("Type a number from 1 to 1024 or 0 to exit: ")
+
+    if not numStr.isnumeric(): continue
+
+    num = int(numStr)
+
+    if num == 0: break
+
+    for i in range(0, num):
+        password += symbols[random.randint(0, len(symbols) - 1)]
+
+    print(password)
+
+paus = str(input("Press <ENTER> to close this windows."))
