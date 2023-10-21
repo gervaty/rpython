7f0682f9432df62601f31afa187458193622af06
Ruslan Sokolov <ruslanmine9@outlook.com>
2023-10-21 12:03:40 +0500

    little change in str2bit

--- str2bit.py
+++ str2bit.py
@@ -2,8 +2,12 @@ def text_to_bits(text, encoding='utf-8', errors='surrogatepass'):
     bits = bin(int.from_bytes(text.encode(encoding, errors), 'big'))[2:]
     return bits.zfill(8 * ((len(bits) + 7) // 8))
 
-print("str2bit - this prigram translate str to ascii")
+print("str 2 bit - this programd translate str to ascii")
 stri = str(input("Enter a string: "))
 
-#text_to_bits(stri)
-print(stri + " in bits: " + text_to_bits(stri))
+bbits = ""
+for i in range(len(stri)):
+    char_bits = text_to_bits(stri[i])
+    bbits += char_bits
+    print(f"Character '{stri[i]}' in bits: {char_bits}")
+print(f"{stri} in bits: {bbits}")
