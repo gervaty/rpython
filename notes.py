380e3eddfd723fb64cbf59671a16d50f05f58dfe
Ruslan <ruslanmine9@outlook.com>
2023-11-18 18:55:14 +0500

    notes.py ver 2

--- notes.py
+++ notes.py
@@ -2,6 +2,7 @@
 #
 #file_path = input("Print file name: ")
 #stringg = input("Print string (your note): ")
+print("notes - this programm adds string to end of file")
 
 file = open(input("Enter a file name: "), "a")
 
