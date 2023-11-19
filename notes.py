3d62c2bc199fde69036f696e0bd9eee6660caecb
Ruslan <ruslanmine9@outlook.com>
2023-11-19 17:36:52 +0500

    notes.py ver 3 but bugs

--- notes.py
+++ notes.py
@@ -2,11 +2,27 @@
 #
 #file_path = input("Print file name: ")
 #stringg = input("Print string (your note): ")
+import os
 print("notes - this programm adds string to end of file")
 
-file = open(input("Enter a file name: "), "a")
+fileforopen = input("Enter a file name: ")
+
+file = open(fileforopen), "r"
 
 string = ""
+lines = 0
+
+for line in file:
+    lines += 1
+
+if lines > 1:
+    print(f"There are {lines} lines in this file")
+if lines < 2:
+    print("There is 1 line in this file")
+
+file.close()
+
+file = open(fileforopen), "a"
 
 while string != "q":
     string = input("Enter a note or q to exit: ")
