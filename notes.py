9dcad4c34c31f25cdb258da3b83f4e1f8e42a252
Ruslan <ruslanmine9@outlook.com>
2023-11-25 19:20:54 +0500

    notes ver 4

--- notes.py
+++ notes.py
@@ -17,29 +17,53 @@ def printFileLinesCount(fileName):
     for line in file:
         lines += 1
     file.close()
-    print("There are", lines, "line(s) in the file.")
+    print("There are", lines, "note(s) in the file.")
 
 def appendLineToFile(fileName, line):
     file = open(fileName, "a")
-    file.write("\n" + line)
+    file.write(line + "\n")
     file.close()
+
+def read_file(fileName):
+    try:
+        with open(fileName, 'r', encoding='utf-8') as file:
+            content = file.read()
+            print(content)
+    except FileNotFoundError:
+        print(f"File {fileName} not found.")
+    except Exception as e:
+        print(f"Error: {e}")
 
 ########################## Main Program ##########################
 
-print("notes - this programm adds string to end of file")
+print("----- notes ver 4 -----")
 
-fileName = input("Enter a file name: ")
+fileName = input("Enter a notes` file name: ")
+
 
 if not ifFileExists(fileName):
     createFile(fileName)
 
 printFileLinesCount(fileName)
 
+openmode = input("Choose a mode [a] - add notes [r] - see notes [q] - quit: ")
+
 string = ""
-while string != "q":
-    string = input("Enter a note or q to exit: ")
-    if string == "q":
-        break
-    appendLineToFile(fileName, string)
+
+if openmode == "a":
+    file = open(fileName, "a")
+
+    while string != "q":
+        string = input("Enter a note or q to exit: ")
+
+        if string == "q":
+            break
+
+        appendLineToFile(fileName, string)
+
+    file.close()
+if openmode == "r":
+    read_file(fileName)
+
 
 a = input("Press <ENTER> to exit")
